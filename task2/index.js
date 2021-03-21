131b58a0fb9018d90a5d3505fc9f6f1433d7234d
ElenaRomanchuk <elena_romanchuk@epam.com>
2021-03-21 09:50:27 +0300

    Task 2. CSV to JSON.

---
+++ task2/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const csv = require('csvtojson');
+const csvFilePath = './task2/csv/nodejs-hw1-ex1.csv';
+const jsonFilePath = './task2/csv/nodejs-hw1-ex1.json';
+
+const errorHandler = (error) => console.error(error);
+
+const readStream = fs.createReadStream(csvFilePath);
+readStream.on('error', errorHandler);
+
+const writeStream = fs.createWriteStream(jsonFilePath);
+writeStream.on('error', errorHandler);
+
+const transformCSVStream = csv({
+  headers: ['book', 'author', 'amount', 'price'],
+  checkType: true,
+});
+transformCSVStream.on('error', errorHandler);
+
+readStream.pipe(transformCSVStream).pipe(writeStream);
